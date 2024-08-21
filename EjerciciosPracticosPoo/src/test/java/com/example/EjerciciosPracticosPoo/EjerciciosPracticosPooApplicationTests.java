82c12f18199c5560ce581a658ca0f4ea40a9664e
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-08-21 17:19:02 -0500

    Add files via upload

---
+++ EjerciciosPracticosPoo/src/test/java/com/example/EjerciciosPracticosPoo/EjerciciosPracticosPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjerciciosPracticosPoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosPracticosPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
