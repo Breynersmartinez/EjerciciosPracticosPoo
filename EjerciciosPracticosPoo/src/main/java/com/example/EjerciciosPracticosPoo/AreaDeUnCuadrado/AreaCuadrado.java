36e5ec002af8647fc0a002d4d65f3ee8af04d922
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-08-21 23:27:59 +0000

    l

---
+++ EjerciciosPracticosPoo/src/main/java/com/example/EjerciciosPracticosPoo/AreaDeUnCuadrado/AreaCuadrado.java
@@ -0,0 +1,8 @@
+package com.example.EjerciciosPracticosPoo.AreaDeUnCuadrado;
+
+import lombok.Data;
+
+@Data
+public class AreaCuadrado {
+
+}
---
+++ EjericiciosPoo/AreaCuadrado.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class AreaCuadrado {
+    public static void main(String[]args)throws Exception{
+Scanner scanner = new Scanner(System.in);
+System.out.println("Ingrese la base del cuadrado ");
+    Double BaseCuadrado = scanner.nextDouble();
+}
+
+
+/*Metodo para calcular el area */
+static Double AreaCuadrado(Double BaseCuadrado){
+return BaseCuadrado*BaseCuadrado;
+}
+}
+
+
+/*https://byspel.com/calcular-dos-sumas-y-su-producto-en-java-utilizando-metodos/ */
