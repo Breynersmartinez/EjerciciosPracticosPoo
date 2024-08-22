a3ea0219c59b9cd5872e90887dc3a26996aa9a5c
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-08-22 16:29:46 +0000

    l

---
+++ EjericiciosPoo/ConversorGrados/ConversorGrados.java
@@ -0,0 +1,18 @@
+ class ConversorGrados {
+private Double grados_c;
+private Double grados_f;
+
+public ConversorGrados(Double grados_c, Double grados_f ){
+    this.grados_c = grados_c;
+    this.grados_f = grados_f;
+}
+
+public Double ConversionCen_Fahr(){
+double grados_f = 0.0;
+return  grados_f = (grados_c * (9.0/5.0)) + 32.0;
+}
+public Double  ConversionFahr_Cen(){
+double grados_c = 0.0;
+return  grados_c = (grados_f - 32.0) * (5.0/9.0);
+}
+}
---
+++ EjericiciosPoo/ConversorGrados/Ejecucion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ejecucion {
+    public static void main(String[] args) throws Exception{
+        
+        Scanner scanner = new Scanner(System.in);
+
+        /* Conversion grados centigrados a fahrenheit */
+        System.out.println(" Ingrese los grados centigrados paraa convertirlos a fahrenheit ");
+        Double grados_c = scanner.nextDouble();
+
+        //Conversion grados  fahrenheit a  centigrados 
+        System.out.println(" Ingrese los grados fahrenheit para convertirlos a centigrados ");
+        Double grados_f = scanner.nextDouble();
+
+        ConversorGrados conversorGrados = new ConversorGrados(grados_c);
+        Double RespuestaConversionCen_fahr = conversorGrados.ConversionCen_Fahr();
+        System.out.println(" Los grados centigrados  convertidos a fahrenheit "+RespuestaConversionCen_fahr);
+        
+       ConversorGrados conversorGrados = new ConversorGrados(grados_f);
+        Double RespuestaConversionCenFahrCen = conversorGrados.ConversionFahr_Cen;
+        System.out.println(" Los grados fahrenheit  convertidos a centigrados " +RespuestaConversionCenFahrCen); */
+
+    }
+}
