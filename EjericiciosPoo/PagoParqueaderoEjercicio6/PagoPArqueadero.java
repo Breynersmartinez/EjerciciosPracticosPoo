a40fc7bfbb5a794af5e769fe39f15f3966b787dc
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-08-23 01:19:47 +0000

    l

--- EjericiciosPoo/PagoParqueaderoEjercicio6/PagoPArqueadero.java
+++ EjericiciosPoo/PagoParqueaderoEjercicio6/PagoPArqueadero.java
@@ -1,3 +1,13 @@
  class PagoPArqueadero {
-    
+    //calcular el pago de un parqueadero por horas. Valor de  la hora $2000 también se debe discriminar el IVA del 20% que se paga
+    private Double Horas;
+
+    public PagoPArqueadero(Double Horas){
+        this.Horas = Horas;
+    }
+
+    public Double CalculoHoras(){
+        int ValorHora=2000;
+        return Horas * ValorHora;
+    }
 }
