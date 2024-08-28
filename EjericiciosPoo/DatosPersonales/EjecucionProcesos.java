ae7437336a1f86ffe43d1a69d0b3ba9fc38217e2
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-08-28 15:59:52 +0000

    l

--- EjericiciosPoo/DatosPersonales/EjecucionProcesos.java
+++ EjericiciosPoo/DatosPersonales/EjecucionProcesos.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 public class EjecucionProcesos {
     public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
+        boolean repetir = false;
+        while(repetir){
+        System.out.println(" Ingrese su nombre ");
+        String nombre = scanner.nextLine();
         System.out.println(" Ingrese su peso en kilogramos ");
         Double peso = scanner.nextDouble();
         System.out.println(" Digite su edad ");
         int edad = scanner.nextInt();
-        System.out.println(" Ingrese su sexo ");
-        String sexo = scanner.nextLine();
+        
         System.out.println( " Ingrese su altura ");
         Double altura = scanner.nextDouble();
+        System.out.println(" Ingrese su sexo ");
+        String sexo = scanner.nextLine();
+
+        //Variable:
+         Double DNI = 0.0;  
+         String hombre = "";
+         String mujer = "";
 
 //ojeto persona 
-        Persona persona = new Persona( peso, edad, sexo, altura );
+        Persona persona = new Persona(  nombre,  edad,  DNI,  sexo,  hombre,  mujer,  peso,  altura );
         Double RespuestaIMC = persona.calcularIMC();
-        if(RespuestaIMC < 20){
+        if(RespuestaIMC <= 20 ){
             System.out.println( " Su peso esta por debajo de su peso ideal ");
         } else if(RespuestaIMC > 25){
             System.out.println( " usted tiene sobre peso ");
@@ -22,7 +32,7 @@ public class EjecucionProcesos {
             System.out.println(" Su peso es ideal con respecto a ssu altura ");
         }
         
-       
+    }
     
 
 
--- EjericiciosPoo/DatosPersonales/Persona.java
+++ EjericiciosPoo/DatosPersonales/Persona.java
@@ -1,33 +1,33 @@
 class Persona {
     private String nombre;
-    private int edad;
-    private Double DNI;
-    private String sexo;
     private String hombre;
     private String mujer;
+    private String sexo;
+    private Double DNI;
     private Double peso;
     private Double altura;
-
-    public Persona(   Double peso, Double altura){  
-        this.peso = peso;
-        this.altura = altura;
-    }
-    public persona(String nombre, int edad, Double DNI, String sexo, String hombre, String mujer){
+    private int edad;
+    
+    public Persona(String nombre, int edad, Double DNI, String sexo, String hombre, String mujer, Double peso, Double altura){
         this.nombre = nombre;
         this.edad = edad;
         this.DNI = DNI;
         this.sexo = sexo;
         this.hombre = hombre;
         this.mujer = mujer;
+        this.peso = peso;
+        this.altura = altura;
+        
     }
+    
  public Double calcularIMC(){
-
-
-    return peso/(altura*altura);
+    return peso/altura*altura;
  }
         
        
 }
+
+
 
 
 
