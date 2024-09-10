aa137c08e739de1c1e91c510d46a029f2195dac1
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-09-10 04:58:00 +0000

    Cambio hotel

--- Taller2_02_06_2024/TiendaFuncionesplus/Producto.java
+++ Taller2_02_06_2024/TiendaFuncionesplus/Producto.java
@@ -3,145 +3,13 @@ import java.util.ArrayList;
 
 
 class Producto{
-    //apublic int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    tributos 
-    private String NombreProducto;
+ 
+    
+    private String nombre;
     private int codigo;
-public c getString() {
-        return String;
-    }
-
-    public Producto(String nombreProducto, int codigo, int cantidad, Double precio) {
-    NombreProducto = nombreProducto;
-    this.codigo = codigo;
-    this.cantidad = cantidad;
-    this.precio = precio;
-}
-
-    public Producto(String nombreProducto, int cantidad, Double precio) {
-        NombreProducto = nombreProducto;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public void setString(c string) {
-        String = string;
-    }
     private int cantidad;
     private Double precio;
     
-    /
-
-    public String getNombreProducto() {
-        return NombreProducto;
-    }
-
-    public void setNombreProducto(String nombreProducto) {
-        NombreProducto = nombreProducto;
-    }
-
-    public int getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }/Constructor
     public Producto( String  NombreProducto, int codigo, int cantidad, Double precio){
         this.NombreProducto = NombreProducto;
         this.codigo = codigo;
@@ -208,15 +76,8 @@ public c getString() {
         }
         return valorTotal;
     }
-    @Override
-    public String toString() {
-        return "Producto [NombreProducto=" + NombreProducto + ", codigo=" + codigo + ", cantidad=" + cantidad
-                + ", precio=" + precio + "]";
-    }
+    public String toString(){
+        return "Producto: { Nombre del producto: "+nombre+", codigo: "+codigo+", cantidad:   "+cantidad+",   precio: "+precio+"}"; 
+     }
 }
 
-@Override
-   public String toString() {
-      return "Producto [NombreProducto=" + NombreProducto + ", codigo=" + codigo + ", cantidad=" + cantidad
-            + ", precio=" + precio + "]";
-   }
