75134e6a7f3405594a7510d62ddf6660249536de
Breynersmartinez <breynersmartinezmunoz@gmail.com>
2024-09-16 10:49:48 -0500

    Suma
    Se añadio el patron de diseño para suma

---
+++ Taller2_pooArreglos/Suma/Entity/NumeroEntity.java
@@ -0,0 +1,22 @@
+package Entity;
+import java.util.*;
+class NumeroEntity
+{
+    //Atributos
+private int valor;
+// construcotr de la clase
+public NumeroEntity ( int valor )
+{
+this.valor = valor;
+}
+
+// Metodos setters and getters 
+public int getValor() {
+    return valor;
+}
+
+public void setValor(int valor) {
+    this.valor = valor;
+}
+
+}
